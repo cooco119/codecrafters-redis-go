acda571d871cc3b014feabf7df474af52217b62a
jason.lee <cooco119@gmail.com>
2021-01-23 00:47:28 +0900

    [#3] Trial 1 - loop for accepting connections

--- app/server.go
+++ app/server.go
@@ -16,13 +16,15 @@ func main() {
 	}
 	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	requestHandler(conn)
+		requestHandler(conn)
+	}
 }
 
 func requestHandler(c net.Conn) {
