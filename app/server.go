57d55d5e5b1e91a43b866bb704b0c7ccef00afe8
jjlee <jjlee@pubg.com>
2021-01-22 23:51:59 +0900

    Running tests

--- app/server.go
+++ app/server.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Your code goes here!")
-
 	// Uncomment this block to pass the first stage
 	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 }
